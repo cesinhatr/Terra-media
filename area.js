141b3333586cb23b7b485bc162d41b0221f600ac
César Rodrigues <cesartr.ufcg@gmail.com>
2025-11-29 10:47:30 -0300

    minha pagina

---
+++ area.js
@@ -0,0 +1,5 @@
+function areaQuadrado() {
+    var lado = prompt("Digite o valor do lado do quadrado:");
+    var area = lado * lado; // ou lado ** 2
+    alert("A área do quadrado é: " + area);
+}
---
+++ funcoes.js
@@ -0,0 +1,5 @@
+function questao() {
+    var metros = prompt("Digite o valor em metros:");
+    var cm = metros * 100;
+    alert(metros + " metros equivalem a " + cm + " centímetros");
+}
